03a7fa87eb64944720e9f6ed0617420c74a4ce92
Benit Mulindwa <110304380+Benitmulindwa@users.noreply.github.com>
2024-01-20 22:15:48 +0100

    Update script.js

--- calculator/script.js
+++ calculator/script.js
@@ -1,1 +1,24 @@
+var display_value = document.getElementById("display");
 
+function append_to_display(input) {
+    display_value.value += input;
+
+
+}
+
+function clear_display() {
+    display_value.value = "";
+
+}
+
+function calculate() {
+    try {
+        result = eval(display_value.value)
+        display_value.value = result
+    }
+    catch (error) {
+        display_value.value = "Error"
+    }
+
+
+}
