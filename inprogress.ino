799700722732e01726590c1a99f7648eb620a209
Benit Mulindwa <110304380+Benitmulindwa@users.noreply.github.com>
2024-02-05 22:08:56 +0100

    Update inprogress.ino

--- inprogress.ino
+++ inprogress.ino
@@ -5,6 +5,15 @@
 #include <ESP8266WebServer.h>
 #include <Hash.h>
 
+
+#define SOUND_SPEED 0.034
+#define BUZZER 2
+
+// Ultrasonic sensor pins
+const int trigPin = 12; // Define Trig pin
+const int echoPin = 14; // Define Echo pin
+int data;
+
 String old_value, value;
 
 ESP8266WiFiMulti    WiFiMulti;
@@ -16,24 +25,69 @@ char html_template[] PROGMEM = R"=====(
    <head>
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1">
-      <title>MrDIY Socket Example</title>
+      <title>Canne Intelligente</title>
       <script>
         socket = new WebSocket("ws:/" + "/" + location.host + ":81");
         socket.onopen = function(e) {  console.log("[socket] socket.onopen "); };
         socket.onerror = function(e) {  console.log("[socket] socket.onerror "); };
         socket.onmessage = function(e) {  
             console.log("[socket] " + e.data);
-            document.getElementById("mrdiy_value").innerHTML = e.data;
+            document.getElementById("mrdiy_value").innerHTML = e.data + " Cm";
+            received_data=e.data;
+            
         };
+
+        let i=0;
+        let distance = parseInt(received_data);
+
+        if(distance<=10){
+          speak("Obstacle inferieur à dix centimetres");
+        }
+
+        function find_canne(){
+
+          if(i%2==0){
+            socket.send(0);
+          }
+          else{
+            socket.send(1);
+          }
+          i=i+1;
+        }
+
+        function speak(my_text){
+
+            if ('speechSynthesis' in window) {
+            
+            var utterance = new SpeechSynthesisUtterance();
+
+            utterance.lang="fr-FR";
+            utterance.text = my_text;
+            speechSynthesis.speak(utterance);
+
+          } else {
+              console.error("Speech synthesis not supported in this browser.");
+          }
+        }
+
+        function say_data(){
+          speak(received_data + "centimetres");
+          
+        }
       </script>
    </head>
    <body style="max-width:400px;margin: auto;font-family:Arial, Helvetica, sans-serif;text-align:center">
-      <div><h1><br />My Value</h1></div>
-      <div><p id="mrdiy_value" style="font-size:100px;margin:0"></p></div>
+      <div><h2><br />Canne Intelligente</h2></div>
+      <div>
+        <button onclick="find_canne()" style="background-color:#cfd0cf; color: black; cursor: pointer; font-size: 20px; font-weight:bold; border-radius: 10px; height: 150px; width:100%; margin: 15px 32px;">Cherche Canne</button>
+      </div>
+      <div><p id="mrdiy_value" style="font-size:60px;margin:0"></p></div>
+      <div>
+        <button onclick="say_data()" style="background-color:#cfd0cf; color: black; cursor: pointer; font-size: 20px; font-weight:bold; border-radius: 10px ; height: 150px; width:100%; margin: 15px 32px;">Distance de la canne</button>
+      </div>
    </body>
 </html>
 )=====";
-
 
 void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length) {
 
@@ -51,7 +105,10 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length
       break;
 
     case WStype_TEXT:
-      Serial.printf("[%u] get Text: %s\n", num, payload);
+      //Serial.printf("[%u] get Text: %s\n", num, payload);
+      data = atoi((char*)payload);
+      digitalWrite(BUZZER, data);
+      //Serial.printf("%s\n",data);
       // send message to client
       // webSocket.sendTXT(num, "message here");
       // send data to all connected clients
@@ -70,10 +127,16 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length
 
 void setup() {
 
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+
+  pinMode(BUZZER, OUTPUT);
+  digitalWrite(BUZZER, 1);
+
   Serial.begin(115200);
   delay(1000);
 
-  WiFiMulti.addAP("xxx", "xxx");
+  WiFiMulti.addAP("Networkname", "password");
 
   while (WiFiMulti.run() != WL_CONNECTED) {
     delay(100);
@@ -98,11 +161,25 @@ void handleNotFound() {
 }
 
 void loop() {
+ 
+//--------------------------------------------------------------------------------------------
+  // ULTRASONIC:
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
   
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  int duration = pulseIn(echoPin, HIGH);
+  int distanceCM = duration*SOUND_SPEED/2;
+//---------------------------------------------------------------------------------------------
+  //COMMUNICATION SERVER->CLIENT
   webSocket.loop();
   server.handleClient();
 
   value = (String) analogRead(0);
-  webSocket.broadcastTXT(value);
+  //Serial.println(value);
+  webSocket.broadcastTXT("13");
   delay(50);
 }
